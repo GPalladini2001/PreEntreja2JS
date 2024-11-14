18ff63fd4868acc22ad2613fe3efca2d27a864b2
Palladini Gabriel <palladinigabriel2001@gmail.com>
2024-11-13 23:58:38 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+hamburguesas = [
+    {
+        carne : "10",
+        pan : "regular",
+        cebolla : true,
+        lechuga : false,
+        quesos: 2,
+        carnes : 2,
+        precio : 10.000
+    },
+    {
+        carne : "4",
+        pan : "cuarto",
+        cebolla : true,
+        lechuga : false,
+        quesos : 2,
+        carnes : 1,
+        precio : 12000
+    }
+]
